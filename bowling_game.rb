348296a923ca46964880662f75019dbf34f0f699
Jim Suchy <jsuchy@gmail.com>
2010-10-12 21:32:16 -0500

    second test

--- bowling_game.rb
+++ bowling_game.rb
@@ -1,11 +1,15 @@
 class BowlingGame
   
+  def initialize
+    @score = 0
+  end
+  
   def roll(pins)
-    
+    @score += pins
   end
   
   def score
-    return 0
+    return @score
   end
   
 end
--- bowling_game_spec.rb
+++ bowling_game_spec.rb
@@ -11,6 +11,16 @@ describe BowlingGame do
     end
     
     @game.score.should == 0
+  end
+  
+  it "scores a game with all 1s" do
+    @game = BowlingGame.new
+    
+    20.times do
+      @game.roll(1)
+    end
+    
+    @game.score.should == 20
   end  
 
 end
