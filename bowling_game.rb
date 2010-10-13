1c330d4d48a5d7f2cc86c93e6ba631fc79d3fedc
Jim Suchy <jsuchy@gmail.com>
2010-10-12 21:44:38 -0500

    third test passing

--- bowling_game.rb
+++ bowling_game.rb
@@ -1,15 +1,26 @@
 class BowlingGame
   
   def initialize
-    @score = 0
+    @rolls = []
   end
   
   def roll(pins)
-    @score += pins
+    @rolls << pins
   end
   
   def score
-    return @score
+    score = 0
+    frame_index = 0
+    10.times do |frame|
+      if @rolls[frame_index] + @rolls[frame_index + 1] == 10
+        score += 10 + @rolls[frame_index + 2]
+        frame_index += 2
+      else
+        score += @rolls[frame_index] + @rolls[frame_index + 1]
+        frame_index += 2
+      end
+    end
+    return score
   end
   
 end
